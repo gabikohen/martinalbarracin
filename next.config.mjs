748611b64d372511daaefd7842bf2e02631b687b
gabokohen <gabokohen@gmail.com>
2025-09-19 14:42:19 -0300

    neuvo cambio

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true, // 👈 fuerza que cada página sea /carpeta/index.html
 };
 
 export default nextConfig;
